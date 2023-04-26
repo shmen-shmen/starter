0438a59c2c669a11f6b133fb2f75034b92c77361
shmen-shmen <akshevchenko@proton.me>
2023-04-26 14:32:46 +0600

    initial commit

---
+++ src/features/cart/cartSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import cartItems from "../../cartItems";
+
+const initialState = {
+	cartItems: cartItems,
+	amount: 1,
+	total: 0,
+	isLoading: true,
+};
+
+const cartSlice = createSlice({
+	name: "cart",
+	initialState,
+	reducers: {
+		clearCart: (state) => {
+			state.cartItems = [];
+		},
+		removeItem: (state, { payload }) => {
+			const itemId = payload;
+			state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+		},
+		increase: (state, { payload }) => {
+			const itemId = payload;
+			const cartItem = state.cartItems.find((item) => item.id === itemId);
+			cartItem.amount = cartItem.amount + 1;
+		},
+		decrease: (state, { payload }) => {
+			const itemId = payload;
+			const cartItem = state.cartItems.find((item) => item.id === itemId);
+			cartItem.amount = cartItem.amount - 1;
+		},
+		calculateTotals: (state) => {
+			let amount = 0;
+			let total = 0;
+			state.cartItems.forEach((item) => {
+				amount += item.amount;
+				total += item.amount * item.price;
+			});
+			state.amount = amount;
+			state.total = total;
+		},
+	},
+});
+
+export const { clearCart, removeItem, increase, decrease, calculateTotals } =
+	cartSlice.actions;
+
+export default cartSlice.reducer;
