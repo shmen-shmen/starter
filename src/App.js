fdacb948900f458d14a737a18dde8a1dc6996af5
shmen-shmen <akshevchenko@proton.me>
2023-04-27 15:43:46 +0600

    cart items are being loaded from API

--- src/App.js
+++ src/App.js
@@ -3,16 +3,29 @@ import CartContainer from "./components/CartContainer";
 import Modal from "./components/Modal";
 
 import { useSelector, useDispatch } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 import { useEffect } from "react";
 
 function App() {
-	const { cartItems } = useSelector((state) => state.cart);
+	const { cartItems, isLoading } = useSelector((state) => state.cart);
 	const { isOpen } = useSelector((state) => state.modal);
 	const dispatch = useDispatch();
+
+	useEffect(() => {
+		dispatch(getCartItems());
+	}, []);
+
 	useEffect(() => {
 		dispatch(calculateTotals());
 	}, [cartItems]);
+
+	if (isLoading) {
+		return (
+			<div className="loading">
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
 
 	return (
 		<main>
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,8 +1,15 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
+
+export const getCartItems = createAsyncThunk("cart/getCartItems", () => {
+	return fetch(url)
+		.then((resp) => resp.json())
+		.catch((error) => console.log(error));
+});
 
 const initialState = {
-	cartItems: cartItems,
+	cartItems: [],
 	amount: 1,
 	total: 0,
 	isLoading: true,
@@ -38,6 +45,19 @@ const cartSlice = createSlice({
 			});
 			state.amount = amount;
 			state.total = total;
+		},
+	},
+
+	extraReducers: {
+		[getCartItems.pending]: (state) => {
+			state.isLoading = true;
+		},
+		[getCartItems.fulfilled]: (state, action) => {
+			state.isLoading = false;
+			state.cartItems = action.payload;
+		},
+		[getCartItems.rejected]: (state) => {
+			state.isLoading = false;
 		},
 	},
 });
